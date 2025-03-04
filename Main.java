99c299d44f942b9252de309c468268ef05730767
Mehar Zain <zainzainpubg1122@gmail.com>
2025-03-04 11:19:58 +0500

    Add initial implementation of Basic class and Java notes notebook

---
+++ Main.java
@@ -0,0 +1,39 @@
+/*
+======================================================
+               ******* ===>> 8 Bit = 1Byte *******
+ * 1 Byte ==> -128 to 127
+ * int ==> 4 Byte
+ * short ==> 16 Bit (2 Byte)
+ * long ==> 64 Bit (8 Byte)
+ * double ==> 
+=======================================================
+ */
+
+// import java.util.Scanner;
+// public class Main{
+// public static void main (String [] arg){
+// int a,b,c;
+// Scanner sc = new Scanner(System.in);
+// System.out.print("Enter Number 1 Value:");
+// a = sc.nextInt();
+// System.out.print("Enter Number 2 Value:");
+// b = sc.nextInt();
+// c = a+b ;
+// System.out.println("Sum is: " + c);
+// sc.close();
+// }
+// }
+
+
+import java.util.*;
+public class Main{
+    public static void main(String []arg){
+        Scanner sc = new Scanner (System.in);
+        
+
+
+
+
+sc.close();
+    }
+}
