55a5f037658bbd768c41918c18f4b29f32ad97e3
Mehar Zain <zainzainpubg1122@gmail.com>
2025-03-05 19:18:34 +0500

    Add age eligibility check to main method

--- Basic.java
+++ Basic.java
@@ -1,5 +1,18 @@
+import java.util.*;
 public class Basic {
     public static void main(String [] args){
-        System.out.println();
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Your Age: ");
+        int a = sc.nextInt();
+        if(a > 18 && a < 900){
+            System.out.println("Congratulation! Your Are Eligible!");
+        }
+        else if(a == 18 ){
+            System.out.println("Congratulation! Your Are Eligible!");
+        }
+        else{
+            System.out.println("Sorry! Your Are Not Eligible!");
+
+        }
     }
 }
