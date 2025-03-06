685ba6a1076a15e43e85f9e4ba91b16ac6cc8606
Mehar Zain <zainzainpubg1122@gmail.com>
2025-03-06 09:45:36 +0500

    z

--- Main.java
+++ Main.java
@@ -28,18 +28,25 @@
 import java.util.*;
 public class Main{
     public static void main(String arg[]){
-        Scanner sc = new Scanner (System.in);
-        System.out.print("Enter First Number: ");
-        int a = sc.nextInt();
-        System.out.print("Enter Second Number: ");
-        int b = sc.nextInt();
-        int c = a+b;
-        System.out.println(c);
+        float num1=(float) 10.5;
+        float num2= (float) 2.0;
+        float sum = num1+num2;
+        float subtraction = num1- num2;
+        float multiplication = num1* num2;
+        float divion = num1/num2;
+        if(num2 <= 0){
+System.out.println("Impossible to Perform Operations");
+        }
+        else{
+
+            System.out.println("Sum : "+sum);
+            System.out.println("Subtraction: "+subtraction);
+            System.out.println("Multiplication: "+multiplication);
+            System.out.println("Divion: "+divion);
+        }
 
 
 
 
-
-sc.close();
     }
 }
