14ada98d3b09c54a6f10f906bd2425cd567b3b5d
Mehar Zain <zainzainpubg1122@gmail.com>
2025-03-06 12:36:39 +0500

    Refactor main method to include student information input and GPA calculations

--- Main.java
+++ Main.java
@@ -25,27 +25,72 @@
 // }
 
 
+// import java.util.*;
+// public class Main{
+//     public static void main(String arg[]){
+//         float num1=(float) 10.5;
+//         float num2= (float) 2.0;
+//         float sum = num1+num2;
+//         float subtraction = num1- num2;
+//         float multiplication = num1* num2;
+//         float divion = num1/num2;
+//         if(num2 <= 0){
+// System.out.println("Impossible to Perform Operations");
+//         }
+//         else{
+
+//             System.out.println("Sum : "+sum);
+//             System.out.println("Subtraction: "+subtraction);
+//             System.out.println("Multiplication: "+multiplication);
+//             System.out.println("Divion: "+divion);
+//         }
+
+
+
+
+//     }
+// }
+
+
+
+
+
+
+
 import java.util.*;
 public class Main{
-    public static void main(String arg[]){
-        float num1=(float) 10.5;
-        float num2= (float) 2.0;
-        float sum = num1+num2;
-        float subtraction = num1- num2;
-        float multiplication = num1* num2;
-        float divion = num1/num2;
-        if(num2 <= 0){
-System.out.println("Impossible to Perform Operations");
-        }
-        else{
+    public static void main(String []args){
+        Scanner sc = new Scanner(System.in);
+System.out.print("Enter Student ID: ");
+int id = sc.nextInt();
+System.out.print("Enter Student GPA: ");
+float gpa = sc.nextFloat();
+sc.nextLine();
+System.out.print("Enter Student Name: ");
+String name = sc.nextLine();
+System.out.print("Enrollement(y/n): ");
+boolean enr = sc.nextBoolean();
+System.out.print("Grade: ");
+char grade = sc.next().charAt(0);
+System.out.println("-------------------------------------------------------------");
+gpa+=0.5;
+System.out.println("GPA After 0.5 using += Operator: "+gpa);
+gpa-=0.2;
+System.out.println("GPA After 0.2 using -= Operator: "+gpa);
+id*=id;
+System.out.print("ID After Double using *= Operator: "+id);
+id/=2;
+System.out.print("ID After Divide using /= Operator: "+id);
+System.out.println("-------------------------------------------------------------");
+id++;
+System.out.println("Student ID After POST-increment Operator: "+id);
+--id;
+System.out.println("Student ID After POST-increment Operator: "+id);
 
-            System.out.println("Sum : "+sum);
-            System.out.println("Subtraction: "+subtraction);
-            System.out.println("Multiplication: "+multiplication);
-            System.out.println("Divion: "+divion);
-        }
 
 
+
+// System.out.println(id +","+ name +","+ enr +","+ grade +","+ gpa);
 
 
     }
