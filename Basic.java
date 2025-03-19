89ee2c0c0ff9741d305c24365bf132a7770fe523
Mehar Zain <zainzainpubg1122@gmail.com>
2025-03-19 19:58:40 +0500

    Remove commented-out main method from Basic class

--- Basic.java
+++ Basic.java
@@ -174,6 +174,40 @@ public class Basic{
 //     }
 // }
 
+// public static void main(String[] args) {
+//     int n=14;
+//         for (int i = 1; i <=n; i++) {
+//             // star - i
+//             for(int j=1;j<=i;j++){
+//                 System.out.print("*");
+//             }
+//             // space - 2*(n-i)
+//             for(int z=1;z<=2*(n-i);z++){
+//                 System.out.print(" ");
+//             }
+//             // star - i
+//             for(int j=1;j<=i;j++){
+//                 System.out.print("*");
+//             }
+//             System.out.println();
+//         }
+//         for (int i = n; i>=1; i--) {
+//             // star - i
+//             for(int j=1;j<=i;j++){
+//                 System.out.print("*");
+//             }
+//             // space - 2*(n-i)
+//             for(int z=1;z<=2*(n-i);z++){
+//                 System.out.print(" ");
+//             }
+//             // star - i
+//             for(int j=1;j<=i;j++){
+//                 System.out.print("*");
+//             }
+//             System.out.println();
+//         }
 
+
+// }
 
 }
