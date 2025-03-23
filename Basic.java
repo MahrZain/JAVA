ca4de91fbe4db235d957f2f147954fe2fd24da10
Mehar Zain <zainzainpubg1122@gmail.com>
2025-03-23 11:15:31 +0500

    Refactor Basic class to replace pair method with subarray method and update main method for demonstration

--- Basic.java
+++ Basic.java
@@ -529,16 +529,53 @@ import java.util.*;
 // }
 
 
+// public class Basic{
+//     public static void pair(int arr[]) {
+//         for (int i = 0; i < arr.length; i++) {
+//             for (int j = i+1; j < arr.length; j++) {
+//                 System.out.print("("+arr[i]+","+arr[j]+")"+" ");
+//             }
+//         }
+//     }
+//     public static void main(String[] args) {
+//         int arr[] = {2,4,6,8,10};
+//         pair(arr);
+//     }
+// }
+
+// public class Basic{
+//     public static void subarray(int arr[]) {
+//         for(int i=0; i<arr.length; i++){
+//             for (int j = i; j <arr.length; j++) {
+//                 for (int k=i; k<=j; k++) {
+//                     System.out.print(arr[k]+" ");
+//                 }
+//                 System.out.println();
+//             }
+//             System.out.println();
+//         }
+//     }
+//     public static void main(String[] args) {
+//         int arr[] = {2,4,6,8,10};
+//         subarray(arr);
+//     }
+// }
+
+
 public class Basic{
-    public static void pair(int arr[]) {
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = i+1; j < arr.length; j++) {
-                System.out.print("("+arr[i]+","+arr[j]+")"+" ");
+    public static void subarray(int arr[]) {
+        for(int i=0; i<arr.length; i++){
+            for (int j = i; j <arr.length; j++) {
+                for (int k=i; k<=j; k++) {
+                    System.out.print(arr[k]+" ");
+                }
+                System.out.println();
             }
+            System.out.println();
         }
     }
     public static void main(String[] args) {
         int arr[] = {2,4,6,8,10};
-        pair(arr);
+        subarray(arr);
     }
 }
