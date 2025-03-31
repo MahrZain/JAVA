74d914f3a562ccbd56c6f61315f55a81dbefaf15
Mehar Zain <zainzainpubg1122@gmail.com>
2025-03-31 19:05:32 +0500

    Comment out main method and class in Basic.java for future reference

--- Basic.java
+++ Basic.java
@@ -860,16 +860,20 @@ import java.util.*;
 
 //     }
 // }
-public class Basic {
-public static void main(String[] args) {
-    int n=50,bin=0,pow=0;
-    while (n>0) {
-        int rem=n%2;
-        bin = bin +( rem*(int)Math.pow(10, pow));
-        pow++;
-        n/=2;
-    }
-    System.out.print(bin);
-}
+// public class Basic {
+// public static void main(String[] args) {
+//     int n=50,bin=0,pow=0;
+//     while (n>0) {
+//         int rem=n%2;
+//         bin = bin +( rem*(int)Math.pow(10, pow));
+//         pow++;
+//         n/=2;
+//     }
+//     System.out.print(bin);
+// }
     
-}
+// }
+
+
+
+
