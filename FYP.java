16a996b2f2ac04f45e1c675ad3c0367753943431
Mehar Zain <zainzainpubg1122@gmail.com>
2025-05-06 11:14:13 +0500

    Comment out unused code and classes in Main.java for future reference

---
+++ FYP.java
@@ -0,0 +1,29 @@
+class webHostingManagementSystem {
+    private String hostingAccount;
+    private String domainName;
+    private String databaseName;
+    private String emailAccount;
+    private String supportTicket;
+    private String hostingPlan;
+    private String paymentMethod;
+    private String paymentStatus;
+    private String supportStatus;
+    private String supportResponseTimeDate;
+
+    // This is a simple Java Web Hosting Management System
+    // It will allow users to manage their web hosting accounts
+    // It will also allow users to manage their domains
+    // It will also allow users to manage their databases
+    // It will also allow users to manage their email accounts
+    // It will also allow users to manage their support
+
+}
+public class FYP {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World! This is my Java Web Hosting Management System.");
+    }
+    
+    
+}
